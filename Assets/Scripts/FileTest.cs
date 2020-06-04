df01e04138f292536e9f24a27879e8bcd3d9ed66
fading <1169061628@qq.com>
2020-06-04 11:46:13 +0800

    改了filetest
    加了几个回车

--- Assets/Scripts/FileTest.cs
+++ Assets/Scripts/FileTest.cs
@@ -8,8 +8,10 @@ public class FileTest : MonoBehaviour
     void Start()
     {
         Debug.Log(Application.dataPath);    //项目的Assets目录
+
         Debug.Log(Application.streamingAssetsPath);//项目目录下的StreamingAssets目录，只读，直接打包进项目，不进行压缩
         Debug.Log(Application.persistentDataPath);//持久化目录，可读可写
+
         Debug.Log(Application.temporaryCachePath);//临时缓存数据的路径
     }
 
